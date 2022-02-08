a0706624adca86f91903bf7d941e250829dd2e3c
HTET KHANT <htet.khant@xenera.jp>
2022-02-08 09:30:53 +0630

    tested some cases at playground

--- TestWithNodejs/Playground/index.js
+++ TestWithNodejs/Playground/index.js
@@ -1,3 +1,46 @@
 // (function test(/* test */) {
 //   console.log("Immediately run function");
 // })();
+
+/*
+const obj = {
+  a: "one",
+  b: "two",
+  a: "three",
+};
+console.log(obj); // { a: 'three', b: 'two' }
+*/
+
+/*
+function getAge(...args) {
+  console.log(typeof args); // object
+}
+getAge(21);
+*/
+
+/*
+const a = {};
+const b = { key: "b" };
+const c = { key: "c" };
+a[b] = 123;
+a[c] = 456;
+console.log(a); // { '[object Object]': 456 }
+console.log(a[b]); // 456
+*/
+
+/*
+function bark() {
+  console.log("woff!");
+}
+bark.animal = "dog";
+console.log(bark); // [Function: bark] { animal: 'dog' }
+console.log(bark.animal); // dog
+*/
+
+/*
+let c = { greeting: "Hey" };
+let d;
+d = c;
+c.greeting = "Hello";
+console.log(d.greeting); // Hello
+*/
